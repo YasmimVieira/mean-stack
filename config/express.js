0539f738dbd574dded67338eff220ddcd989cec4
Yasmim Barbosa Vieira <yasmim.vieira@aluno.faculdadeimpacta.com.br>
2020-09-13 21:23:42 -0300

    chore: create jwt authentication

--- config/express.js
+++ config/express.js
@@ -3,10 +3,17 @@ const consign = require("consign");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.set("secret", "autenticando");
+
 app.use(express.static("./public"));
 
 app.use(bodyParser.json());
 
-consign({ cwd: "app" }).include("models").then("api").then("routes").into(app);
+consign({ cwd: "app" })
+  .include("models")
+  .then("api")
+  .then("routes/auth.js")
+  .then("routes")
+  .into(app);
 
 module.exports = app;
